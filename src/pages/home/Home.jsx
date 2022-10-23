f90b72064de036f6959259f0dac7e25ad41bbab4
khaled Reza <khaledreza@gmail.com>
2022-10-23 21:25:32 +0200

    added top bar component ui design

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import "./home.css";
+import Topbar from "../../components/topbar/Topbar";
+
+const home = () => {
+  return (
+    <div>
+      <Topbar />
+    </div>
+  );
+};
+
+export default home;
