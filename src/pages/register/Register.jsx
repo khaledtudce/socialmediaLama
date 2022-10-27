982932fde199aaa77db91d12c139861ec7eba314
khaled Reza <khaledreza@gmail.com>
2022-10-27 02:47:12 +0200

    added register page ui design - finished UI design part

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,7 +1,31 @@
 import "./register.css";
 
 const Register = () => {
-  return <div>Register</div>;
+  return (
+    <div className="register">
+      <div className="registerWrapper">
+        <div className="registerLeft">
+          <h3 className="registerLogo">Kaynat Social</h3>
+          <span className="registerDesc">
+            Connect with friends and the world around you on Kaynat Social
+          </span>
+        </div>
+        <div className="registerRight">
+          <div className="registerBox">
+            <input className="registerInput" placeholder="username"></input>
+            <input className="registerInput" placeholder="email"></input>
+            <input className="registerInput" placeholder="password"></input>
+            <input
+              className="registerInput"
+              placeholder="password again"
+            ></input>
+            <button className="registerButon">Sign Up</button>
+            <button className="registerRegisterButton">Log Into Account</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
