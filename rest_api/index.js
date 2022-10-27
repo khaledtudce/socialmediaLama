2d5ea7cb5cee62b5f74a1b0d0442e599e4c62a08
khaled Reza <khaledreza@gmail.com>
2022-10-27 16:37:48 +0200

    added dotenv file and MongoDb Connection

--- rest_api/index.js
+++ rest_api/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+  console.log("Connected to MondoDb");
+});
 
 app.listen(8800, () => {
-  console.log("Backend server is ready");
+  console.log("Backend server is ready! ");
 });
