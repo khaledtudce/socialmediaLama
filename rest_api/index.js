a8b4b1a63b30c3600aa79664aba085ead11314ee
khaled Reza <khaledreza@gmail.com>
2022-10-29 20:27:57 +0200

    added auth route for registration, login, password compare using bcrypt, added user route with create, update, delete, follow, unfollow option using mongodb

--- rest_api/index.js
+++ rest_api/index.js
@@ -5,6 +5,7 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
@@ -17,6 +18,7 @@ app.use(helmet());
 app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
   res.send("Welcome to homepage");
--- rest_api/routes/users.js
+++ rest_api/routes/users.js
@@ -1,7 +1,111 @@
 const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
 
-router.get("/test", (req, res) => {
+router.get("/", (req, res) => {
   res.send("hey its userRoute");
+});
+
+//update user
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+
+      try {
+        const updateUser = await User.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          {
+            new: true,
+          }
+        );
+        res.status(200).json(updateUser);
+      } catch (error) {
+        res.status(500).json(error);
+      }
+    }
+  } else {
+    res.status(403).json("You can update only your account");
+  }
+});
+
+//delete user
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("User id: " + req.params.id + "has been deleted");
+    } catch (error) {
+      res.status(500).json("Could not delete the user: " + req.params.id);
+    }
+  } else {
+    res.status(403).json("You can delete only your account");
+  }
+});
+
+//get a user
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+    res.status(200).json(other);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+//follow user
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const userToFollow = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (!userToFollow.followers.includes(req.body.userId)) {
+        await userToFollow.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json("User is successfully followed");
+      } else {
+        res.status(403).json("User has been followed already");
+      }
+    } catch (error) {
+      res.status(403).json(error);
+    }
+  } else {
+    res.status(500).json("You cannot follow yourself");
+  }
+});
+
+//unfollow user
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const userToUnfollow = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (userToUnfollow.followers.includes(req.body.userId)) {
+        await userToUnfollow.updateOne({
+          $pull: { followers: req.body.userId },
+        });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json("User is successfully unfollowed");
+      } else {
+        res
+          .status(403)
+          .json("User has not been followed. So, you cannot unfollow");
+      }
+    } catch (error) {
+      res.status(403).json(error);
+    }
+  } else {
+    res.status(500).json("You cannot unfollow yourself");
+  }
 });
 
 module.exports = router;
