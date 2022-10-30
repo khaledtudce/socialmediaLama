fd138035700c3c38119a77a1253d930e1c56b3d0
khaled Reza <khaledreza@gmail.com>
2022-10-30 13:12:13 +0100

    added react router dom v6.4.0 and applied it

--- src/App.js
+++ src/App.js
@@ -2,9 +2,33 @@ import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
-  return <Profile />;
+  console.log("hello");
+  const loggedInUser = false;
+  const isNewRegistered = false;
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route
+            path="/register"
+            element={loggedInUser ? <Navigate to="/" /> : <Register />}
+          />
+
+          <Route
+            path="/login"
+            element={
+              loggedInUser || isNewRegistered ? <Navigate to="/" /> : <Login />
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
